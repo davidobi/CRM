f800b214e8170be821a7890c173e88138a98d0c1
David <david.obi@github.com>
2016-12-29 19:54:06 +0000

    Modified the Alphabetized and missing email files, they now use the index view

--- app/controllers/finder_controller.rb
+++ app/controllers/finder_controller.rb
@@ -1,5 +1,20 @@
 class FinderController < ApplicationController
 	def index
+		@page_title = "All Customers"
 		@customers = Customer.all
 	end
+
+	def alphabetized
+		@page_title = "Customers in Alphebetical Order"
+		@customers = Customer.order(:full_name)
+		render :index
+	end
+
+	def missing_email
+		@page_title = "Customers without Email Addresses"
+		@customers = Customer.where(email_address: '')
+		render :index
+
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'finder#index'
 
+
+  get 'alphabetized' => 'finder#alphabetized', as: 'alphabetized'
+  get 'missing_email' => 'finder#missing_email', as: 'missing_email'
+
 end
